e418291d60e2f962eb4af2381d8426f40b6c4eea
growingsprout <jewong08270827@gmail.com>
2025-04-09 16:26:17 +0900

    final

--- main.ino
+++ main.ino
@@ -3,7 +3,25 @@
 #include "esp_wifi.h"
 #include "esp_mac.h"
 #include "esp_chip_info.h"
+#include <HTTPClient.h>
+//......................................
+const char* ssid = "KWIC-DEMO-ROOM";
+const char* password = "1234Qwer";
 
+// Firebase Functions 배포 후 받은 URL
+const char* firebaseUrl = "https://us-central1-siscodb-23511.cloudfunctions.net/deviceHandler";
+
+// 버튼 핀 설정 (예: GPIO0)
+//const int buttonPin = 13;//---------------------------------------------------------------------------
+
+//bool lastButtonState = LOW;//---------------------------------------------------------------------------
+
+// Potentiometer is connected to GPIO 34 (Analog ADC1_CH6) 
+const uint16_t potPin = 34;
+// variable for storing the potentiometer value
+uint16_t potValue = 0, lastpotValue = 0;
+
+//......................................
 // Beacon Packet buffer
 uint8_t packet[128] = { 
   0x80, 0x00,             // Frame Control
@@ -23,28 +41,52 @@ char ssids[ssidnum][32] = {
 };
 
 bool broadcasting = false;
-String serialCommand;
+//String serialCommand;
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  
+void beacon_mode() {
   WiFi.mode(WIFI_MODE_AP);
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   esp_wifi_init(&cfg);
   esp_wifi_set_storage(WIFI_STORAGE_RAM);
   esp_wifi_set_mode(WIFI_MODE_AP);
   esp_wifi_start();
-  
-  Serial.println("Beacon Spam Tool");
-  Serial.println("Commands:");
-  Serial.println("start - Start broadcasting");
-  Serial.println("stop - Stop broadcasting");
-  Serial.println("list - List current SSIDs");
-  //Serial.println("set <position> <name> - Set new SSID (position 0-9)");
-  Serial.println("status - Show current status");
 }
 
+void button_mode() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\n✅ WiFi connected");
+}
+
+void setup() {
+  Serial.begin(115200);
+  //pinMode(buttonPin, INPUT_PULLDOWN);
+  beacon_mode();
+  delay(100);
+  button_mode();
+
+  //delay(1000);
+  
+  //WiFi.mode(WIFI_MODE_AP);
+  //wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  //esp_wifi_init(&cfg);
+  //esp_wifi_set_storage(WIFI_STORAGE_RAM);
+  //esp_wifi_set_mode(WIFI_MODE_AP);
+  //esp_wifi_start();
+  
+  //Serial.println("Beacon Spam Tool");
+  //Serial.println("Commands:");
+  //Serial.println("start - Start broadcasting");
+  //Serial.println("stop - Stop broadcasting");
+  //Serial.println("list - List current SSIDs");
+  //Serial.println("set <position> <name> - Set new SSID (position 0-9)");
+  //Serial.println("status - Show current status");
+}
+/*
 void handleSerialCommands() {
   if (Serial.available()) {
     serialCommand = Serial.readStringUntil('\n');
@@ -70,7 +112,7 @@ void handleSerialCommands() {
       Serial.print("Broadcasting: ");
       Serial.println(broadcasting ? "Active" : "Stopped");
     }
-    /*
+    
     else if (serialCommand.startsWith("set ")) {
       int pos = serialCommand.substring(4, 5).toInt();
       String newSSID = serialCommand.substring(6);
@@ -84,12 +126,56 @@ void handleSerialCommands() {
       } else {
         Serial.println("Invalid position or SSID length");
       }
-    }*/
+    }
   }
 }
-
+*/
 void loop() {
-  handleSerialCommands();
+  //handleSerialCommands();
+  broadcasting = true;//
+
+  potValue = (analogRead(potPin))/10;
+  
+  
+  //bool buttonState = digitalRead(buttonPin);//---------------------------------------------------------------------------
+
+  if (potValue>30 && lastpotValue<20) { //1st pin:409, 2nd pin : 228, 3rd pin : 113, 4th pin : 36
+    Serial.println("🔘 Button pressed!");
+
+    // MAC 주소 얻기
+    uint8_t mac[6];
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+    char macStr[18];
+    sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X",
+            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    Serial.print("MAC: ");
+    Serial.println(macStr);
+
+    // Firebase Function 호출
+    if (WiFi.status() == WL_CONNECTED) {
+      String actions = "??"; // 4
+      if (potValue>350) actions = "register"; // 1
+      else if (potValue>150) actions = "delete"; // 2
+      else if (potValue>50) actions = "danger"; // 3
+      HTTPClient http;
+      http.begin(firebaseUrl);
+      http.addHeader("Content-Type", "application/json");
+      
+      String body = "{\"action\":\"" + actions + "\",\"mac\":\"" + String(macStr) + "\",\"deviceId\":\"esp32-001\"}";
+      Serial.println(body);
+      int httpResponseCode = http.POST(body);
+
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      Serial.println(http.getString());
+
+      http.end();
+    }
+
+    delay(300); // 디바운스 대기
+  }
+
+  lastpotValue = potValue;//---------------------------------------------------------------------------
   
   if (broadcasting) {
     for(int i = 0; i < ssidnum; i++) {
